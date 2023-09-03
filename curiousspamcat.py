ceba4fe091bd4bacd566bd86070fadf8ce6c6388
pachimaroo <143965859+pachimaroo@users.noreply.github.com>
2023-09-03 14:20:10 -0600

    Update curiousspamcat.py

--- curiousspamcat.py
+++ curiousspamcat.py
@@ -4,14 +4,14 @@ random.seed = (os.urandom(1024))
 
 url='https://curiouscat.me/api/v2/post/create'
 
-personid=input("Curiouscat name: ")
-sleeptime=int(input("Sleep time (seconds): "))
+personid=input("Curiouscat name:MoralOrelClay")
+sleeptime=int(input("Sleep time (seconds):100"))
 
 while True  :
     text="".join( [random.choice(string.ascii_letters+string.digits) for i in range(random.randint(15,50))] )
 
     requests.post(url, allow_redirects=False, data={
-        "to": personid,
+        "to": MoralOrelClay,
         "anon": "true",
         "question": text
     })
